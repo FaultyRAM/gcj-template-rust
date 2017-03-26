a713d694d2813336ba888c676193d758308425eb
FaultyRAM <explodingpsu@gmail.com>
2017-03-26 18:17:39 +1000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+extern crate gcj_helper;
+
+use gcj_helper::TestCases;
+
+fn main() {
+    let tc = TestCases::new();
+    tc.run(|tc_io| {
+               // Your code goes here.
+           });
+}
