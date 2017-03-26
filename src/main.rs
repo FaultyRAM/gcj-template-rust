ebb849dd4d1d7e93a899df11fbae481b32a62b99
FaultyRAM <explodingpsu@gmail.com>
2017-03-26 23:03:59 +1000

    Updated for new revision

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 extern crate gcj_helper;
 
-use gcj_helper::TestCases;
+use gcj_helper::TestEngine;
 
 fn main() {
-    let tc = TestCases::new();
-    tc.run(|tc_io| {
-               // Your code goes here.
-           });
+    TestEngine::new("./foo.in", "./foo.out").run(|io_helper| {
+        // Your code goes here.
+    });
 }
