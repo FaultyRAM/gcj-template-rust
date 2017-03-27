c424acf95f73202262ede77fd5cced84149701ca
FaultyRAM <explodingpsu@gmail.com>
2017-03-28 01:04:20 +1000

    Updated for `gcj-helper` 0.3

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 extern crate gcj_helper;
 
 use gcj_helper::TestEngine;
+use std::io::{Read, Write};
 
 fn main() {
-    TestEngine::new("./foo.in", "./foo.out").run(|io_helper| {
+    TestEngine::new("./foo.in", "./foo.out").run(|input, output| {
         // Your code goes here.
     });
 }
